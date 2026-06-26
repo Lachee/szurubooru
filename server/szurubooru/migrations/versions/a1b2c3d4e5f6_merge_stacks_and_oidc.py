17d8903212831367d3041ae749f0636972e082c5
Lachee <lake.hopkins@outlook.com>
2026-06-26 12:26:57 +1000

    Added a mgiration to combine the unrelated heads

---
+++ server/szurubooru/migrations/versions/a1b2c3d4e5f6_merge_stacks_and_oidc.py
@@ -0,0 +1,19 @@
+"""
+Merge post stacks and oidc branches
+
+Revision ID: a1b2c3d4e5f6
+Created at: 2026-06-26 00:01:00.000000
+"""
+
+revision = "a1b2c3d4e5f6"
+down_revision = ("f17a8e3c90b2", "516093f0ad52")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
